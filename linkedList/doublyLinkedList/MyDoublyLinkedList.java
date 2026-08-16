59b1e4f0b81547ad4b334d02a1f6c03ae725b9ac
Awanish Singh <awanish.singh@sita.aero>
2026-08-16 17:08:56 +0530

    doubly linkedlist added

---
+++ linkedList/doublyLinkedList/MyDoublyLinkedList.java
@@ -0,0 +1,47 @@
+package linkedList.linkedList.doublyLinkedList;
+
+import java.util.Objects;
+
+public class MyDoublyLinkedList<E> {
+	int size = 0;
+	Node<E> head;
+	
+	public void add(E data) {
+		Node<E> node = new Node<E>(data);
+		if(head == null) {
+			head = node;
+			size++;
+			return;
+		}
+		Node<E> current = head;
+		while(current.next != null) {
+			current = current.next;
+		}
+		current.next = node;
+		node.prev = current;
+		size++;
+	}
+	
+	public void removeLast() {
+		if(Objects.isNull(head)) {
+			System.out.println("Empty list");
+			return;
+		}
+		if(head.next == null) {
+			head = null;
+			size--;
+			return;
+		}
+		Node<E> current = head;
+		while(current.next != null) {
+			current = current.next;
+		}
+		Node<E> prev = current.prev;
+		prev.next = null;
+		current.prev = null;
+		size--;
+		
+	}
+	
+	
+}
